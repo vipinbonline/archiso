5dac96953228f6800a7e872bfcd5f51aa8544a6b
Vipin Balakrishnan <vipinbonline@gmail.com>
2018-09-04 00:37:53 +0530

    shell script added

---
+++ documents/customize_airootfs.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e -u
+
+sed -i 's/#\(en_US\.UTF-8\)/\1/' /etc/locale.gen
+locale-gen
+
+ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
+
+usermod -s /usr/bin/ssh root
+
+cp -aT /etc/skel/ /root/
+chmod 700 /root
+
+sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
+sed -i 's/#\(Storage=\)auto/\1volatile/' /etc/systemd/journald.conf
+
+sed -i 's/#\(HandleSuspendKey=\)suspend/\1ignore/' /etc/systemd/logind.conf
+sed -i 's/#\(HandleHibernateKey=\)hibernate/\1ignore/' /etc/systemd/logind.conf
+sed -i 's/#\(HandleLidSwitch=\)suspend/\1ignore/' /etc/systemd/logind.conf
+
+
+systemctl enable pacman-init.service choose-mirror.service 
+
+
+#systemctl set-default multi-user.target
+systemctl set-default graphical.target
+
+useradd -m demo
+sed -i '/^root/a demo ALL=(ALL) ALL' /etc/sudoers
+echo "demo:demo" | chpasswd  # echo "<username>:<password>"
+
+
